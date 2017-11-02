18bf8b23d13deaa3f2bea464ba9530f2f2d35e9c
Rustam Kalimulin <kalimulin-ru@yandex.ru>
2017-11-02 10:43:12 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  div
+    .container-fluid.navbar.navbar-dark.bg-dark
+          .navbar-nav.ml-auto
+            .form-inline.d-flex.justify-content-end
+              div
+                button.btn.btn-outline-success.mr-3(type="button", @click="switchSign('sign-in')") Войти
+                button.btn.btn-outline-success(type="button", @click="switchSign('sign-up')") Регистрация
+    .container
+      .row
+        .col
+          sign-in(v-if="sign === 'sign-in'")
+          sign-up(v-else="")
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      sign: 'sign-in'
+    }
+  },
+  components: {
+    SignIn,
+    SignUp
+  },
+  methods: {
+    switchSign(currentSign) {
+      this.sign = currentSign;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
